40493e1f30e3e7433d0b25e4f0cfa38da07c47be
Massimo Ginella <143848693+tinymassi@users.noreply.github.com>
2024-04-25 06:25:14 +0000

    added stack, mazewidth, height, comments

--- maze_main.cpp
+++ maze_main.cpp
@@ -11,8 +11,31 @@ public:
         // this allows the string to handle unicode characters (something with windows API)
     }
 
+private:
+    int m_nMazeWidth;  // stores width of maze
+    int m_nMazeHeight;  // stores height of maze
+    int *m_maze;  // dynamic array that stores values for all cells to store neighbors each cell is connected to
+    // a dynamic array is basically just an array that changes its size dynamically during run time
+
+    enum {  // enum is short for enumeration. user defined data type that contains constant vals
+        CELL_PATH_N = 0x01,  // each cell has a bitmask (in hex notation) represents its state
+        CELL_PATH_E = 0x02,  // Represents 00000010 in binary
+        CELL_PATH_S = 0x04,  // Represents 00000100 in binary
+        CELL_PATH_W = 0x08,
+        CELL_VISITED = 0x10,
+    };
+
+    int m_nVisitedCells;  // keeps track of the cells visited
+
+    stack<std::pair<int, int>> m_stack; // stack that keeps track of movement throughout the maze
+    //  the pair is to store the (x,y) coords of the maze grid
+
 protected:  // different from 'private' as stuff in 'protected' can be accessed by classes derived from this class
     virtual bool OnUserCreate() {  // virtual creates a universal function that can be modified by other classes
+        // Maze Parameters
+        m_nMazeWidth = 40;
+        m_nMazeHeight = 25;
+
         return true;
     }
 
