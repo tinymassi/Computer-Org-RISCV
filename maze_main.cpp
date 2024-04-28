8f3122a3f3d8467fffe9b5f51db18d0c40525a57
Massimo Ginella <143848693+tinymassi@users.noreply.github.com>
2024-04-28 06:29:23 +0000

    Added 2D maze array and maze traversal console output

--- maze_main.cpp
+++ maze_main.cpp
@@ -36,10 +36,33 @@ protected:  // different from 'private' as stuff in 'protected' can be accessed 
         m_nMazeWidth = 40;
         m_nMazeHeight = 25;
 
+        m_maze = new int[m_nMazeHeight * m_nMazeWidth];  // dynamically allocates memory for 2D array of integers based on area of maze
+        memset(m_maze, 0x00, m_nMazeWidth * m_nMazeHeight);  // set all elements of array to zero
+        // 'memset' essentially fills a block of memory with a particular value
+
+        m_stack.push(std::make_pair(0, 0));  // set the starting position in the maze
+        m_maze[0] = CELL_VISITED;  // set the first element of the 2D maze array to already visited
+        m_nVisitedCells = 1;  // initially at 0,0 so we have visited 1 cell so far
+
         return true;
     }
 
     virtual bool OnUserUpdate(float fElapsedTime) {
+
+        // Clear Screen
+        Fill(0, 0, m_nScreenWidth, m_nScreenHeight, L' ');
+
+        // Draw Maze
+        for (int x = 0; x < m_nscreenWidth; x++) {  // move through maze width
+            for (int y = 0; y < m_nScreenHeight, y++) {  // move through maze height
+                if (m_maze[y * m_nMazeWidth + x] & CELL_VISITED) {  // if the cell real & is visited
+                    Draw(x, y, PIXEL_SOLID, FG_WHITE)  // draw a white box at position (x, y)
+                } else {  // if the cell is not visited
+                    Draw(x, y, PIXEL_SOLID, FG_BLUE)  // draw a blue box at the positon (x, y)
+                }
+            }
+        }
+
         return true;
     }
 
